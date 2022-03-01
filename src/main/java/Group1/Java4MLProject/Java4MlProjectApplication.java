902bfba00002f68df4cf3ebb9732842fd84b3fed
Asmaasa3d <asmaasaeed289@gmail.com>
2022-03-01 21:38:40 +0200

    initial version

---
+++ src/main/java/Group1/Java4MLProject/Java4MlProjectApplication.java
@@ -0,0 +1,13 @@
+package Group1.Java4MLProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4MlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java4MlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Group1/Java4MLProject/Java4MlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Group1.Java4MLProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4MlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
